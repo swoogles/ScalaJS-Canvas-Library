356e70235d5344fd7cb5aa9a227676efd72df73f
bfrasure <bill@solutionreach.com>
2016-03-27 20:34:59 -0600

    Don't make new Random for every invocation  #time 5m

--- src/main/scala/shared/CollectionFunctions.scala
+++ src/main/scala/shared/CollectionFunctions.scala
@@ -3,9 +3,9 @@ package shared
 import scala.util.Random
 
 object CollectionFunctions {
+  val rand = new Random(System.currentTimeMillis());
 
   def randomElement[T](list: List[T]): T = {
-    val rand = new Random(System.currentTimeMillis());
     val random_index = rand.nextInt(list.length);
     list(random_index)
   }
